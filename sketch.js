7779b317c6c1aa7629363466e43f16bdeea84416
collinkip <collinskliplangat3@gmail.com>
2022-10-04 09:17:14 +0300

    snake game init

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var s;
+scl=20;
+var food;
+
+function setup() {
+  createCanvas(400,400);
+  s = new Snake();
+  frameRate(10);
+  picklocation();
+}
+function picklocation(){
+  var cols=floor(width/scl);
+  var rows=floor(height/scl) ;
+  food=createVector(floor(random(cols)),floor(random(rows)));
+  food.mult(scl);
+}
+function draw() {
+  background(50);
+  s.update();
+  s.show();
+  if (s.eat(food)) {
+    console.log("truee")
+  };
+ 
+
+  fill(255,0,100);
+  rect(food.x,food.y,scl,scl)
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    s.dir(0,-1);
+  }
+  else if(keyCode === DOWN_ARROW){
+    s.dir(0,1);
+  }
+  else if(keyCode === RIGHT_ARROW){
+    s.dir(1,0);
+  }
+  else if(keyCode === LEFT_ARROW){
+    s.dir(-1,0);
+  }
+}
